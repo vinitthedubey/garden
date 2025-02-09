573deda09defd18c5c363cf70b8e49b5772a8859
vinit <vinit.data2002@gmail.com>
2025-02-09 17:47:14 +0530

    added

--- app.py
+++ app.py
@@ -690,5 +690,5 @@ def subscribe():
     return "Success", 200  # OK response
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", 5000))  # Use the assigned PORT or default to 5000
+    port = 5000
     app.run(host="0.0.0.0", port=port)
